a43e66d9331e13ea0bd71403971da1b8310981f9
Sivamedisetti <96729473+Sivamedisetti@users.noreply.github.com>
2022-01-14 16:24:11 +0530

    #include<stdio.h> #include<math.h> int main() {     int n, i;     scanf("%d",&n);     for(i=0;i<=n;i++)     {         if(n==i*i)         {             printf("True");             return 0;         }     }
            printf("False");
            return 0;
        
    }

---
+++ perfect square root or not.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n, i;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(n==i*i)
+        {
+            printf("True");
+            return 0;
+        }
+    }
+        
+        printf("False");
+        return 0;
+    
+}
